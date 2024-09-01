98ad6e8d1c7286b473d92715f6f9844455f23196
Siddharth Kanoujia <130286366+SiDdHaRtHKaNoUjIa13@users.noreply.github.com>
2024-09-01 15:15:26 +0530

    Time: 2 ms (70.65%), Space: 8.7 MB (45.68%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,29 +1,47 @@
 class Solution {
 public:
-    string addStrings(string num1, string num2) {
-    
 
-        int i= num1.length()-1;
-        int j= num2.length()-1;
-        int carry =0;
+    string add(string num1, string num2){
+
         string ans;
+        int index1=num1.size()-1;
+        int index2 = num2.size()-1;
+        int sum =0;
+        int carry =0;
 
-        while(i>=0 || j>=0|| carry>0){
-
-            if(i>=0){
-                carry = carry + num1[i] -'0';
-                i--;
-            }
-            if(j>=0){
-                carry = carry + num2[j]-'0';
-                j--;
-            }
-
-            ans = ans + char(carry%10+'0');
-            carry= carry/10;
+        while(index2>=0){
+            sum = (num1[index1] -'0') + (num2[index2] -'0') + carry;
+            carry = sum/10;
+            char ch = sum%10 +'0';
+            ans += ch;
+            index1--;
+            index2--;
         }
-        reverse(ans.begin(),ans.end());
+        // now index1 remains 
+        while(index1>=0){
+            sum = (num1[index1] -'0') + carry;
+            carry = sum/10;
+            char ch = sum%10 +'0';
+            ans += ch;
+            index1--;
+            
+        }
+
+        if(carry){
+            ans+= '1';
+            
+        }
+        reverse(ans.begin(), ans.end());
         return ans;
 
+    }
+    string addStrings(string num1, string num2) {
+        
+        if(num1.size()<num2.size()){
+            return add(num2,num1);
+        }
+        else{
+            return add(num1,num2);
+        }
     }
 };
