9df66fc3e17418c11cecc515e0a9712f397bb718
Siddharth Kanoujia <130286366+SiDdHaRtHKaNoUjIa13@users.noreply.github.com>
2024-11-12 08:21:04 +0530

    Time: 0 ms (100%), Space: 9.5 MB (5.51%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        
+        int i =0;
+        int j =0;
+        string result;
+        while(i<word1.size() || j<word2.size()){
+            if(i<word1.size()){
+                result = result + word1[i];
+                i++;
+            }
+            if(j<word2.size()){
+                result = result + word2[j];
+                j++;
+            }
+        }
+        return result;
+            
+    }
+};
